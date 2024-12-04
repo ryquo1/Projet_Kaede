c74a5d5201fc1e96af372a71193fe64a97f02f90
Valentin SNITH <valentin.snith@etu.unicaen.fr>
2024-12-04 08:47:45 +0100

    Add : Login functions (not working at the moment)

--- app/Http/Controllers/KaeUsersController.php
+++ app/Http/Controllers/KaeUsersController.php
@@ -4,12 +4,30 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 
 use App\Models\web\KaeUsers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KaeUsersController extends Controller
 {
-    public static function getAllSource(){
+    public static function getAllSource()
+    {
         return view('user', ["users" => KaeUsers::all()]);
+    }
+
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/great');
+        }
+
+        return redirect('/login')->with('error', 'VOUS AVEZ ÉCHOUÉ ! Mais vous pouvez toujours réessayer...');
     }
 }
